bdd44e7ce41adfdadc93f3ebdf50fc11e74c990a
Kaung Htet Zaw <me@kaunghtet.xyz>
2018-12-24 03:06:15 +0800

    Pad single digit minutes

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+const padZero = num => `0${num}`.slice(-2);
+
 const showTime = () => {
   const now = new Date();
   const hour = now.getHours();
   const minutes = now.getMinutes();
-  document.getElementById('clock').innerHTML = `${hour}:${minutes}`;
+  document.getElementById('clock').innerHTML = `${hour}:${padZero(minutes)}`;
   setTimeout(showTime, 60000);
 }
 
